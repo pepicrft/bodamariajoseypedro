2a78714f8660452100058066ec567d48d1d9d57e
Pedro Piñera <pedro@ppinera.es>
2019-04-28 22:04:38 +0200

    Change description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Boda de María José y Pedro`,
+    description: `Información de la boda de María José y Pedro el próximo 21 de septiembre`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
